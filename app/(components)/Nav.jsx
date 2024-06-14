b7cd217a2b75ae4328cc579a8ca0a4fe4412e22c
Moeez Abdul <moeezrajput2000@gmail.com>
2024-06-14 16:04:02 +0100

    NavBar Design Implemented

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,7 +1,23 @@
+import { faHome, faListCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
 const Nav = () => {
     return (
-        <div>Nav</div>
+        <nav className="flex justify-between bg-nav p-4">
+            <div className="flex items-center space-x-4">
+                <Link href="/">
+                    <FontAwesomeIcon icon={faHome} className="icon" />
+                </Link>
+                <Link href="/TaskPage/new">
+                    <FontAwesomeIcon icon={faListCheck} className="icon" />
+                </Link>
+            </div>
+            <div>
+                <p className="text-default-text">Example@gmail.com</p>
+            </div>
+        </nav>
     )
-}
+};
 
 export default Nav
