c01de3306d984c74926ee69f2b2a23de8a944cea
Moeez Abdul <moeezrajput2000@gmail.com>
2024-06-14 19:48:33 +0100

    Create Task Form Design Completed

--- app/(components)/TaskForm.jsx
+++ app/(components)/TaskForm.jsx
@@ -14,20 +14,28 @@ const TaskForm = () => {
     }));
   };
 
+  const handleSubmit = () => {
+    console.log("SUBMITTED");
+  };
+
   const startTaskData = {
     title: "",
     description: "",
     priority: 1,
     progress: 0,
     status: "Not Defined",
-    category: "Software Products",
+    category: "Interview Preparation",
   };
 
   const [formData, setFormData] = useState(startTaskData);
 
   return (
     <div className="flex justify-center bg-card">
-      <form>
+      <form
+        className="flex flex-col gap-3 w-1/2"
+        method="post"
+        onSubmit={handleSubmit}
+      >
         <h3>Create Your Task</h3>
         <label>Title</label>
         <input
@@ -38,6 +46,102 @@ const TaskForm = () => {
           required={true}
           value={formData.title}
         />
+
+        <label>Description</label>
+        <textarea
+          id="description"
+          name="description"
+          onChange={handleChange}
+          required={true}
+          value={formData.description}
+          rows="5"
+        />
+
+        <label>Category</label>
+        <select
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+        >
+          <option value="Interview Preparation">Interview Preparation</option>
+          <option value="Team Meeting">Team Meeting</option>
+          <option value="Grocery List">Grocery List</option>
+          <option value="Course">Course Started</option>
+          <option value="Shopping List">Shopping List</option>
+        </select>
+
+        <label>Priority</label>
+        <div>
+          <input
+            type="radio"
+            id="priority-1"
+            name="priority"
+            onChange={handleChange}
+            value={1}
+            checked={formData.priority == 1}
+          />
+          <label>1</label>
+
+          <input
+            type="radio"
+            id="priority-2"
+            name="priority"
+            onChange={handleChange}
+            value={2}
+            checked={formData.priority == 2}
+          />
+          <label>2</label>
+
+          <input
+            type="radio"
+            id="priority-3"
+            name="priority"
+            onChange={handleChange}
+            value={3}
+            checked={formData.priority == 3}
+          />
+          <label>3</label>
+
+          <input
+            type="radio"
+            id="priority-4"
+            name="priority"
+            onChange={handleChange}
+            value={4}
+            checked={formData.priority == 4}
+          />
+          <label>4</label>
+
+          <input
+            type="radio"
+            id="priority-5"
+            name="priority"
+            onChange={handleChange}
+            value={5}
+            checked={formData.priority == 5}
+          />
+          <label>5</label>
+        </div>
+
+        <label>Progress</label>
+        <input
+          type="range"
+          id="progress"
+          name="progress"
+          value={formData.progress}
+          min="0"
+          max="100"
+          onChange={handleChange}
+        />
+
+        <label>Status</label>
+        <select name="status" value={formData.status} onChange={handleChange}>
+          <option value="Uncompleted">Uncompleted</option>
+          <option value="In Progress">In Progress</option>
+          <option value="Completed">Completed</option>
+        </select>
+
+        <input type="submit" className="btn text-white" value="Create" />
       </form>
     </div>
   );
