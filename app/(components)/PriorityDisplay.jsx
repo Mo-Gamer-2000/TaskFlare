42b8706d1085e7292b34b6c8ec261c27f458062e
Moeez Abdul <moeezrajput2000@gmail.com>
2024-06-15 11:56:52 +0100

    Dynamically rendering task priority with flame icons progress

--- app/(components)/PriorityDisplay.jsx
+++ app/(components)/PriorityDisplay.jsx
@@ -1,17 +1,32 @@
-import { faFire } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { faFire } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-const PriorityDisplay = () => {
-    return (
-        <div className="flex justify-start align-baseline">
-            <FontAwesomeIcon icon={faFire} className='text-red-400' />
-            <FontAwesomeIcon icon={faFire} className='text-red-400' />
-            <FontAwesomeIcon icon={faFire} className='text-red-400' />
-            <FontAwesomeIcon icon={faFire} className='text-red-400' />
-            <FontAwesomeIcon icon={faFire} className='text-red-400' />
-        </div>
-    )
-}
+const PriorityDisplay = ({ priority }) => {
+  return (
+    <div className="flex justify-start align-baseline">
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 0 ? "text-yellow-400" : "text-white"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 1 ? "text-yellow-400" : "text-white"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 2 ? "text-yellow-400" : "text-white"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 3 ? "text-yellow-400" : "text-white"}`}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 4 ? "text-yellow-400" : "text-white"}`}
+      />
+    </div>
+  );
+};
 
-export default PriorityDisplay
+export default PriorityDisplay;
